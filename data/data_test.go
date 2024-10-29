c7d92e9dcfbf014d0503cd559c01e5d5b3a88231
Kevin <kjrodgers26@gmail.com>
2024-10-28 22:21:16 -0400

    updated test logic and add another edge case check

--- data/data_test.go
+++ data/data_test.go
@@ -1,52 +1,59 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 	"time"
-	"errors"
 )
-
 
 func TestDaysBetweenTasks(t *testing.T) {
 	testCases := []TestData{
 		{
 			Data: Data{
-				ID: "1",
-				Task: "Move furniture",
-				Created: time.Date(2024,10,12,0,0,0,0,time.UTC), 
-				Done: false,
+				ID:      "1",
+				Task:    "Move furniture",
+				Created: time.Date(2024, 10, 12, 0, 0, 0, 0, time.UTC),
+				Done:    false,
 			},
 			expected_days: 16,
 		},
 		{
 			Data: Data{
-				ID: "2",
-				Task: "Walk Dog",
-				Created: time.Date(2024,9,12,0,0,0,0,time.UTC), 
-				Done: false,
+				ID:      "2",
+				Task:    "Walk Dog",
+				Created: time.Date(2024, 9, 12, 0, 0, 0, 0, time.UTC),
+				Done:    false,
 			},
 			expected_days: 46,
 		},
 		{
 			Data: Data{
-				ID: "3",
-				Task: "Run a mile",
-				Created: time.Date(2023,10,28,0,0,0,0,time.UTC), 
-				Done: false,
+				ID:      "3",
+				Task:    "Run a mile",
+				Created: time.Date(2023, 10, 28, 0, 0, 0, 0, time.UTC),
+				Done:    false,
 			},
-			expected_days: 365,
+			expected_days: 366,
 		},
 		{
 			Data: Data{
-				ID: "4",
-				Task: "Buy groceries",
-				Created: time.Date(2025,10,12,0,0,0,0,time.UTC), 
-				Done: false,
+				ID:      "4",
+				Task:    "Run two mile",
+				Created: time.Date(2024, 10, 28, 0, 0, 0, 0, time.UTC),
+				Done:    false,
 			},
-			expected_error: errors.New("you can't create a task from the future... unlesssssss... nevermind"),
+			expected_days: 0,
 		},
-
+		{
+			Data: Data{
+				ID:      "5",
+				Task:    "Buy groceries",
+				Created: time.Date(2025, 10, 12, 0, 0, 0, 0, time.UTC),
+				Done:    false,
+			},
+			expected_error: errors.New("you can't create a task from the future...unlesssssss...nevermind"),
+		},
 	}
 
 	pass := 0
@@ -55,8 +62,9 @@ func TestDaysBetweenTasks(t *testing.T) {
 	for _, test := range testCases {
 		actualDays, err := test.DaysBetweenTasks()
 		fmt.Println("---------------------------------------------")
-	if test.expected_error != nil && test.expected_error == err {
-			fmt.Printf(`
+		if test.expected_error != nil {
+			if err == nil || test.expected_error.Error() != err.Error() {
+				t.Errorf(`
 ID:             %s
 Task:           %s
 Created:        %v
@@ -65,11 +73,11 @@ Expected Days:  %d
 Actual Days:    %d
 Expected Error: %v
 Actual Erorr:   %v`,
-		test.ID, test.Task, test.Created, test.Done, test.expected_days, actualDays, test.expected_error, err)
-			fmt.Println("\nTest Passed")
-			pass++
-		} else if test.expected_error != nil && test.expected_error != err {
-			t.Errorf(`
+					test.ID, test.Task, test.Created, test.Done, test.expected_days, actualDays, test.expected_error, err)
+				fmt.Println("\nTest Failed")
+				fail++
+			} else {
+				fmt.Printf(`
 ID:             %s
 Task:           %s
 Created:        %v
@@ -78,9 +86,10 @@ Expected Days:  %d
 Actual Days:    %d
 Expected Error: %v
 Actual Erorr:   %v`,
-		test.ID, test.Task, test.Created, test.Done, test.expected_days, actualDays, test.expected_error, err)
-			fmt.Println("\nTest Failed")
-			fail++
+					test.ID, test.Task, test.Created, test.Done, test.expected_days, actualDays, test.expected_error, err)
+				fmt.Println("\nTest Passed")
+				pass++
+			}
 		} else if test.expected_error == nil && test.expected_days == actualDays {
 			fmt.Printf(`
 ID:             %s
@@ -91,11 +100,11 @@ Expected Days:  %d
 Actual Days:    %d
 Expected Error: %v
 Actual Erorr:   %v`,
-		test.ID, test.Task, test.Created, test.Done, test.expected_days, actualDays, test.expected_error, err)
+				test.ID, test.Task, test.Created, test.Done, test.expected_days, actualDays, test.expected_error, err)
 			fmt.Println("\nTest Passed")
 			pass++
 		} else {
-				t.Errorf(`
+			t.Errorf(`
 ID:             %s
 Task:           %s
 Created:        %v
@@ -104,7 +113,7 @@ Expected Days:  %d
 Actual Days:    %d
 Expected Error: %v
 Actual Erorr:   %v`,
-		test.ID, test.Task, test.Created, test.Done, test.expected_days, actualDays, test.expected_error, err)
+				test.ID, test.Task, test.Created, test.Done, test.expected_days, actualDays, test.expected_error, err)
 			fmt.Println("\nTest Failed")
 			fail++
 		}
@@ -120,3 +129,4 @@ Actual Erorr:   %v`,
 	fmt.Printf("Passed: %d  Failed: %d\n", pass, fail)
 
 }
+
