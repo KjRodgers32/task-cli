335a156116e8b0e23a8bb73330e44c938c385bac
Kevin <kjrodgers26@gmail.com>
2024-10-28 23:31:57 -0400

    added uniform printing to get task statments; starting to feel good about go now :)

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/KjRodgers32/task-cli/data"
 )
 
@@ -17,31 +18,35 @@ func main() {
 		fmt.Scanln(&response)
 
 		switch response {
-			case "1":
-				tasks, err := data.GetTasks()
-				if err != nil {
-					fmt.Printf("there was an error retrieving your tasks: %v\n", err)
-					return
-				}
-
-				fmt.Println("ID			Task			Created			Done")
-
-				for _, task := range tasks {
-					daysPassed, err := task.DaysBetweenTasks()
-					if err != nil {
-						fmt.Printf("there was an error calculating days between tasks: %v", err)
-					}
-					fmt.Printf("%s %s %d days ago %v\n", task.ID, task.Task, daysPassed, task.Done)
-				}
-
-			case "2":
-				data.CreateTasks()
-			case "3":
-				data.DeleteTasks()
-			case "4":
+		case "1":
+			tasks, err := data.GetTasks()
+			if err != nil {
+				fmt.Printf("there was an error retrieving your tasks: %v\n", err)
 				return
-			default:
-				fmt.Println("Please select a vaild response...")
+			}
+
+			colPadLength, err := data.MaxColLength(tasks)
+			if err != nil {
+				fmt.Printf("there was a problem printing your tasks: %v\n", err)
+			}
+			fmt.Printf("%-*s %-*s %-*s %-*s\n", colPadLength, "ID", colPadLength, "Task", colPadLength, "Created", colPadLength, "Done")
+			for _, task := range tasks {
+				daysPassed, err := task.DaysBetweenTasks()
+				if err != nil {
+					fmt.Printf("there was an error calculating days between tasks: %v", err)
+				}
+				fmt.Printf("%-*s %-*s %-*s %-*v\n", colPadLength, task.ID, colPadLength, task.Task, colPadLength, fmt.Sprintf("%d days ago", daysPassed), colPadLength, task.Done)
+			}
+
+		case "2":
+			data.CreateTasks()
+		case "3":
+			data.DeleteTasks()
+		case "4":
+			return
+		default:
+			fmt.Println("Please select a vaild response...")
 		}
 	}
 }
+
