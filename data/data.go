424d9fd5a2f2c03f5a5279b459738c25f13b1fe1
Kevin <kjrodgers26@gmail.com>
2024-10-28 22:57:39 -0400

    added helper methods

--- data/data.go
+++ data/data.go
@@ -1,11 +1,31 @@
 package data
 
 import (
-	"fmt"
-	"os"
 	"encoding/csv"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
 	"time"
 )
+
+func Max(nums ...int) int {
+	largest := int(math.Inf(-1))
+	for _, num := range nums {
+		if num > largest {
+			largest = num
+		}
+	}
+	return largest
+}
+
+func maxColLength(tasks []Data) int {
+	var maxLength int
+	for _, task := range tasks {
+		maxLength = Max(maxLength, len(task.ID), len(task.Task), len(task.Created.Format("2006-01-02")), len(strconv.FormatBool(task.Done)))
+	}
+	return maxLength
+}
 
 func GetTasks() ([]Data, error) {
 	file, err := os.Open("task.csv")
@@ -34,16 +54,16 @@ func GetTasks() ([]Data, error) {
 		}
 
 		var task Data
+
 		task.ID = row[0]
 		task.Task = row[1]
-		fmt.Println(row[2])
-		date, err := time.Parse("2006-01-02",row[2])
+		date, err := time.Parse("2006-01-02", row[2])
 		if err != nil {
 			return nil, err
 		}
+
 		task.Created = date
 		fmt.Sscan(row[3], &task.Done)
-
 		tasks = append(tasks, task)
 	}
 
@@ -51,7 +71,7 @@ func GetTasks() ([]Data, error) {
 }
 
 func CreateTasks() {
-	return 
+	return
 }
 
 func DeleteTasks() {
