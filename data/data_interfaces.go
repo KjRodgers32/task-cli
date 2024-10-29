080a219b4bcfd3e1f94378ff3ef3994cfd15dfdd
Kevin <kjrodgers26@gmail.com>
2024-10-28 22:20:27 -0400

    updated logic for methods

--- data/data_interfaces.go
+++ data/data_interfaces.go
@@ -1,25 +1,26 @@
 package data
 
 import (
-	"time"
-	"math"
 	"errors"
+	"math"
+	"time"
 )
 
 func (d *Data) DaysBetweenTasks() (int, error) {
 	timeInBetween := time.Since(d.Created)
-	if timeInBetween.Hours() <= 24.0 {
-		return 0, errors.New("you can't create a task from the future... unlesssssss... nevermind")
+	if timeInBetween.Hours() <= 0 {
+		return 0, errors.New("you can't create a task from the future...unlesssssss...nevermind")
 	} else {
-		return int(math.Floor(timeInBetween.Hours() / 24)), nil
+		return int(math.Floor(timeInBetween.Hours()/24)) - 1, nil
 	}
 }
 
 func (td *TestData) DaysBetweenTasks() (int, error) {
 	timeInBetween := time.Since(td.Created)
-	if timeInBetween.Hours() <= 24.0 {
-		return 0, errors.New("you can't create a task from the future... unlesssssss... nevermind")
+	if timeInBetween.Hours() <= 0 {
+		return 0, errors.New("you can't create a task from the future...unlesssssss...nevermind")
 	} else {
-		return int(math.Floor(timeInBetween.Hours() / 24)), nil
+		return int(math.Floor(timeInBetween.Hours()/24)) - 1, nil
 	}
 }
+
